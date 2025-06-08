f0c065396f49951ee6f66cdf74824744719f26b8
JemAndrew <149887251+JemAndrew@users.noreply.github.com>
2025-06-08 19:02:28 +0100

    Fix service worker cache paths

--- assets/service-worker.js
+++ assets/service-worker.js
@@ -1,29 +1,33 @@
 self.addEventListener('install', event => {
-    event.waitUntil(
-      caches.open('holiday-cluedo-cache').then(cache => {
-        return cache.addAll([
-          './',
-          './index.html',
-          './mission.html',
-          './game.html',
-          './update.html',
-          './style.css',
-          './setup.js',
-          './mission.js',
-          './game.js',
-          './update.js',
-          './manifest.json',
-          './static/logo.png'
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', event => {
-    event.respondWith(
-      caches.match(event.request).then(response => {
-        return response || fetch(event.request);
-      })
-    );
-  });
+  event.waitUntil(
+    caches.open('holiday-cluedo-cache').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './mission.html',
+        './game.html',
+        './update.html',
+        './assets/style.css',         
+        './scripts/setup.js',     
+        './scripts/mission.js',        
+        './scripts/game.js',    
+        './scripts/update.js',         
+        './assets/manifest.json',       
+        './assets/logo.png'             
+      ]);
+    })
+  );
+});
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
   
